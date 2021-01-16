bdb70ea8b627335e8dec74bd6a99982bdbc3e94d
Engin Celal Taşkın <engin48ect@gmail.com>
2021-01-17 00:37:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MaasHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                Console.WriteLine("Lütfen adınızı giriniz : ");
+                string Ad = Console.ReadLine();
+                Console.WriteLine("\nÇalıştığınız saati giriniz: ");
+                int saat = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nÇalıştığınız saatlik ücreti giriniz: ");
+                int sUcret = Convert.ToInt32(Console.ReadLine());
+
+                Maas calisan = new Maas(Ad, saat, sUcret);
+                            
+            }
+            catch
+            {
+                Console.WriteLine("Verilere uygun değer girilmedi.\nSistem sonlandırılıyor...");
+            }
+            
+        }
+    }
+
+    class Maas
+    {
+        public Maas(string adi, int calisilanSaat, int saatlikUcret)
+        {
+            int maas = calisilanSaat * saatlikUcret;
+            Console.WriteLine("\nİsim: " + adi);
+            Console.WriteLine("Aldığınız maaş: " + maas);
+        }
+        
+    }
+}
